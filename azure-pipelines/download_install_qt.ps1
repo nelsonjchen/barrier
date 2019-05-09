e4f38f7952ff36ec869d2cd4a48e845b4db95c19
Nelson Chen <crazysim@gmail.com>
2019-05-09 07:56:36 -0700

    Tighten up some of the install scripts

--- azure-pipelines/download_install_qt.ps1
+++ azure-pipelines/download_install_qt.ps1
@@ -1,11 +1,14 @@
-$Wc = New-Object System.Net.WebClient  
-$Wc.DownloadFile('https://download.qt.io/official_releases/qt/5.12/5.12.3/qt-opensource-windows-x86-5.12.3.exe', 'qt.exe') ; 
-Write-Output 'Downloaded QT Installer' 
-$Env:QT_INSTALL_DIR = Join-Path (Get-Location) "deps" | Join-Path -ChildPath "Qt"  | Join-Path -ChildPath "Qt5.12.3"  ; 
-Write-Output 'Starting QT Installer' 
-Start-Process qt.exe -ArgumentList '--verbose --script azure-pipelines/qtifwsilent.qs' -NoNewWindow -Wait 
-Write-Output 'Installed QT Installer' 
-Remove-Item qt.exe -Force 
-Remove-Item $Env:QT_INSTALL_DIR\MaintenanceTool.exe -Force 
-Write-Output 'Deleted Maintenance Tooling' 
+$ErrorActionPreference = "Stop"
+
+New-Item -Force -ItemType Directory -Path ".\deps\"
+$Wc = New-Object System.Net.WebClient
+$Wc.DownloadFile('https://download.qt.io/official_releases/qt/5.12/5.12.3/qt-opensource-windows-x86-5.12.3.exe', 'deps\qt.exe') ;
+Write-Output 'Downloaded QT Installer'
+$Env:QT_INSTALL_DIR = Join-Path (Get-Location) "deps\Qt\Qt5.12.3"
+Write-Output 'Starting QT Installer'
+Start-Process deps\qt.exe -ArgumentList '--verbose --script azure-pipelines/qtifwsilent.qs' -NoNewWindow -Wait
+Write-Output 'Installed QT Installer'
+Remove-Item deps\qt.exe -Force
+Remove-Item $Env:QT_INSTALL_DIR\MaintenanceTool.exe -Force
+Write-Output 'Deleted Maintenance Tooling'
 
