38d3610525601b57acc3fee7dcbc0138a627d276
Nelson Chen <crazysim@gmail.com>
2019-05-09 01:05:40 -0700

    OK if folder doesn't exist

--- azure-pipelines/download_install_bonjour_sdk_like.ps1
+++ azure-pipelines/download_install_bonjour_sdk_like.ps1
@@ -5,7 +5,7 @@ $Wc = New-Object System.Net.WebClient
 $Wc.DownloadFile('https://github.com/nelsonjchen/mDNSResponder/releases/download/v2019.05.08.1/x64_RelWithDebInfo.zip', 'deps\BonjourSDKLike.zip') ; 
 Write-Output 'Downloaded BonjourSDKLike Installer' 
 Write-Output 'Starting BonjourSDKLike Installer' 
-Remove-Item -Recurse -Force .\deps\BonjourSDKLike
+Remove-Item -Recurse -Force -ErrorAction Ignore .\deps\BonjourSDKLike 
 Expand-Archive .\deps\BonjourSDKLike.zip -DestinationPath .\deps\BonjourSDKLike
 Write-Output 'Installed BonjourSDKLike Installer' 
 Remove-Item deps\BonjourSDKLike.zip
