614c16c853997473c5df6b72100b2847b8f05012
Nelson Chen <crazysim@gmail.com>
2019-05-09 00:38:21 -0700

    Save QT to a path structure like on a normal system

--- azure-pipelines/download_install_qt.ps1
+++ azure-pipelines/download_install_qt.ps1
@@ -1,7 +1,7 @@
 $Wc = New-Object System.Net.WebClient  
 $Wc.DownloadFile('https://download.qt.io/official_releases/qt/5.12/5.12.3/qt-opensource-windows-x86-5.12.3.exe', 'qt.exe') ; 
 Write-Output 'Downloaded QT Installer' 
-$Env:QT_INSTALL_DIR = Join-Path (Get-Location) "Qt"; 
+$Env:QT_INSTALL_DIR = Join-Path (Get-Location) "deps" | Join-Path -ChildPath "Qt"  | Join-Path -ChildPath "Qt5.12.3"  ; 
 Write-Output 'Starting QT Installer' 
 Start-Process qt.exe -ArgumentList '--verbose --script azure-pipelines/qtifwsilent.qs' -NoNewWindow -Wait 
 Write-Output 'Installed QT Installer' 
