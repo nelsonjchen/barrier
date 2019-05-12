a2e0d1709a370978ad9ac880b41d42cefa492846
Nelson Chen <crazysim@gmail.com>
2019-05-12 10:35:33 -0700

    Initial QLI Installer

--- azure-pipelines/download_install_qt.ps1
+++ azure-pipelines/download_install_qt.ps1
@@ -1,14 +1,23 @@
 $ErrorActionPreference = "Stop"
 
-New-Item -Force -ItemType Directory -Path ".\deps\"
-$Wc = New-Object System.Net.WebClient
-$Wc.DownloadFile('https://download.qt.io/official_releases/qt/5.12/5.12.3/qt-opensource-windows-x86-5.12.3.exe', 'deps\qt.exe') ;
-Write-Output 'Downloaded QT Installer'
-$Env:QT_INSTALL_DIR = Join-Path (Get-Location) "deps\Qt\Qt5.12.3"
-Write-Output 'Starting QT Installer'
-Start-Process deps\qt.exe -ArgumentList '--verbose --script azure-pipelines/qtifwsilent.qs' -NoNewWindow -Wait
-Write-Output 'Installed QT Installer'
-Remove-Item deps\qt.exe -Force
-Remove-Item $Env:QT_INSTALL_DIR\MaintenanceTool.exe -Force
-Write-Output 'Deleted Maintenance Tooling'
+$qli_install_version = '2019.05.12.1'
+$qt_version = '5.12.3'
 
+New-Item -Force -ItemType Directory -Path ".\deps\"
+Write-Output 'Downloading QLI Installer'
+$Wc = New-Object System.Net.WebClient
+
+$Wc.DownloadFile("https://github.com/nelsonjchen/qli-installer/archive/v$qli_install_version.zip", 'qli-installer.zip') ;
+Write-Output 'Downloaded QLI Installer'
+Write-Output 'Extracting QLI Installer'
+Expand-Archive qli-installer.zip deps\
+Move-Item .\deps\qli-installer-$qli_install_version\ .\deps\qli-installer
+Write-Output 'Extracted QLI Installer'
+Write-Output 'Installing QLI Installer Dependencies'
+pip install -r .\deps\qli-installer\requirements.txt
+Write-Output 'Installed QLI Installer Dependencies'
+Write-Output 'Starting QT Installer'
+python .\deps\qli-installer\qli-installer.py $qt_version windows desktop win64_msvc2017_64
+New-Item -Force -ItemType Directory -Path ".\deps\Qt\Qt$qt_version\$qt_version"
+Move-Item .\deps\qli-installer\output\$qt_version ".\deps\Qt\Qt$qt_version\$qt_version"
+Write-Output 'Installed QT Installer'
