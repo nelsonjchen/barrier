9b7d26ac87d8940b74a2858f1453f2c3482eac29
Nelson Chen <crazysim@gmail.com>
2019-05-12 11:59:01 -0700

    Adjust paths

--- azure-pipelines/download_install_qt.ps1
+++ azure-pipelines/download_install_qt.ps1
@@ -7,11 +7,11 @@ New-Item -Force -ItemType Directory -Path ".\deps\"
 
 Write-Output 'Downloading QLI Installer'
 $Wc = New-Object System.Net.WebClient
-$Wc.DownloadFile("https://github.com/nelsonjchen/qli-installer/archive/v$qli_install_version.zip", 'qli-installer.zip') ;
+$Wc.DownloadFile("https://github.com/nelsonjchen/qli-installer/archive/v$qli_install_version.zip", '.\deps\qli-installer.zip') ;
 Write-Output 'Downloaded QLI Installer'
 
 Write-Output 'Extracting QLI Installer'
-Expand-Archive qli-installer.zip deps\
+Expand-Archive deps\qli-installer.zip deps\
 Move-Item .\deps\qli-installer-$qli_install_version\ .\deps\qli-installer
 Write-Output 'Extracted QLI Installer'
 
@@ -20,6 +20,6 @@ pip install -r .\deps\qli-installer\requirements.txt
 Write-Output 'Installed QLI Installer Dependencies'
 
 Write-Output 'Starting QT Installer'
-$Env:QLI_OUT_DIR = ".\deps\Qt\Qt$qt_version\$qt_version"
+$Env:QLI_OUT_DIR = ".\deps\Qt\Qt$qt_version"
 python .\deps\qli-installer\qli-installer.py $qt_version windows desktop win64_msvc2017_64
 Write-Output 'Installed QT Installer'
